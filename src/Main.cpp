e9ab2b07d887c27f0741192fa0312e68944aa525
Aidan Chelig <aidan@forester.io>
2018-12-26 21:40:57 -0600

    yus

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,59 @@
-#include <iostream>
-using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main(){
-  cout << "helllnlo wrld";
-  return 0;
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+
+    if (!glfwInit())
+        return -1;
+
+    glewInit();
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+      std::cout << "could not inialize glew!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+      -0.5f, -0.5f,
+       0.0f,  0.5f,
+       0.5f, -0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float));
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
