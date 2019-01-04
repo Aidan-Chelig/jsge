22ef0ae3ba412c6caf99b1d4a7e99d533c852b0f
Jaden Giordano <thejadenjack@gmail.com>
2019-01-03 21:11:57 -0500

    Cleanup and make buildable on OSX

---
+++ src/Game.h
@@ -0,0 +1,33 @@
+#ifndef Game_h
+#define Game_h
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include <stdio.h>
+#include <iostream>
+
+class Game {
+  
+public:
+  Game();
+  ~Game();
+
+void init(const char* title, int xpos, int ypos, int height, int width, bool fullscreen);
+  
+  void handleEvents();
+  void update();
+  void render();
+  void clean();
+
+  bool running() { return isRunning ;}
+
+
+private:
+  bool isRunning;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+
+
+};
+
+#endif /* Game_hpp */
