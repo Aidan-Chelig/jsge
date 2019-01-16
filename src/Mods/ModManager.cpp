61a587ac37a838ffbc5fe5a708a0e8cdb5f4fddd
aidan chelig <aidan.chelig@gmail.com>
2019-01-16 16:26:30 -0600

    build script now symlinks mods folder

--- src/Mods/ModManager.cpp
+++ src/Mods/ModManager.cpp
@@ -9,38 +9,6 @@ ModManager::~ModManager(){
 }
 
 
- void parseMods(const char* directory){
-  DIR *dir;
-  struct dirent *ent;
-  int folderCount = 0;
-  int index = 0;
-  const char* tempName;
-  std::vector<std::string> modDirs;
-  if ((dir = opendir (directory)) != NULL) {
-    while ((ent = readdir (dir)) != NULL) {
-      index++;
-      if(index < 3)// the first 2 directorys are always . and .. ignore them
-       continue; 
-
-      tempName = ent->d_name;
-      std::cout << tempName << std::endl;
-      modDirs.push_back(std::string(tempName));
-      //create array of mod struct
-      folderCount++;
-    }
-    //loop again but this time populate the mod
-    closedir(dir);
-  } else {
-    perror ("");
-  }
-}
-
 void ModManager::Init(){
-  char buff[FILENAME_MAX];
-  GetCurrentDir( buff, FILENAME_MAX );
-  std::string current_working_dir(buff);
-  std::cout << current_working_dir << std::endl;
-  std::string mod_dir = current_working_dir + "/Mods";
 
-  parseMods(mod_dir.c_str());
 }
